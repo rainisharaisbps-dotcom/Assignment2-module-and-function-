a7d9672814fe9248097e4bba1d860103535397c8
rainisharaisbps-dotcom <rainisharaisbps@gmail.com>
2025-08-16 11:04:36 +0530

    Add files via upload

---
+++ factorial task 1.py
@@ -0,0 +1,8 @@
+import math
+number = int(input('enter your number'))
+if number <=0 :
+    print('please enter Valid number')
+else :
+    print("the root of number is:", math.sqrt(number))
+    print("the lagarithm of nuber is :",math.log(number))
+    print("the sine of nuber is :",math.sin(number))
